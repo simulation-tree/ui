dd9a0c5a8442fa1a3c870963f2c2b6e0c64221ee
Phill <philldamaskin@gmail.com>
2025-01-03 09:06:20 -0500

    destroy canvas when target is destroyed

--- source/Systems/CanvasSystem.cs
+++ source/Systems/CanvasSystem.cs
@@ -5,6 +5,7 @@ using Simulation;
 using System;
 using System.Numerics;
 using Transforms.Components;
+using Unmanaged;
 using Worlds;
 
 namespace InteractionKit.Systems
@@ -17,6 +18,9 @@ namespace InteractionKit.Systems
 
         void ISystem.Update(in SystemContainer systemContainer, in World world, in TimeSpan delta)
         {
+            USpan<uint> destroyedCanvases = stackalloc uint[64];
+            uint destroyedCanvasCount = 0;
+
             ComponentQuery<IsCanvas, Position, Scale> canvasQuery = new(world);
             foreach (var x in canvasQuery)
             {
@@ -29,6 +33,12 @@ namespace InteractionKit.Systems
                 if (cameraEntity != default && world.ContainsEntity(cameraEntity))
                 {
                     Camera camera = new(world, cameraEntity);
+                    if (camera.IsDestroyed())
+                    {
+                        destroyedCanvases[destroyedCanvasCount++] = canvasEntity;
+                        continue;
+                    }
+
                     Destination destination = camera.Destination;
                     if (destination != default && !destination.IsDestroyed())
                     {
@@ -43,6 +53,11 @@ namespace InteractionKit.Systems
 
                 ref Scale scale = ref x.component3;
                 scale.value = new(size, scale.value.Z);
+            }
+
+            for (uint i = 0; i < destroyedCanvasCount; i++)
+            {
+                world.DestroyEntity(destroyedCanvases[i]);
             }
         }
 
