d875dec795accd40f8da02fbc028f27aee25830d
Phill <philldamaskin@gmail.com>
2025-02-13 21:05:19 -0500

    add draggable entity type

---
+++ source/Draggable.cs
@@ -0,0 +1,13 @@
+﻿using UI.Components;
+using Worlds;
+
+namespace UI
+{
+    public readonly partial struct Draggable : IEntity
+    {
+        readonly void IEntity.Describe(ref Archetype archetype)
+        {
+            archetype.AddComponentType<IsDraggable>();
+        }
+    }
+}
